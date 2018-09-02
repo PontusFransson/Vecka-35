8b3e1c49cdb0994bb2344d7dc2e3e9c48a85546f
PontusFransson <33531126+PontusFransson@users.noreply.github.com>
2018-09-02 16:52:41 +0200

    uppg 1

--- src/vecka35/uppg1.java
+++ src/vecka35/uppg1.java
@@ -4,16 +4,22 @@ public class uppg1{
 
 	public static void main(String[] args) {
 		
-	
+		System.out.println(nummersumma(1,2,3,4,123213,213,12312,3,123,123,21,31,312,312,312,321,3,123,213));
 		
 	}
 	
 	public static int nummersumma(int... siffror){
 		
-		return 0;
+		int nummersumma = 0;
+		
+		for (int i : siffror) {
+			nummersumma += i;
+		}
+		
+		return nummersumma; 
 		
 	}
-		
 	
-		
+	
+	
 }

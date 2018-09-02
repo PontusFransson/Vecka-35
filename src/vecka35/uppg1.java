b51d94bed16db0573f86c4ac8c21719563db2c92
PontusFransson <pontus.fransson.te16d@gmail.com>
2018-09-02 16:40:55 +0200

    1

---
+++ src/vecka35/uppg1.java
@@ -0,0 +1,19 @@
+package vecka35;
+
+public class uppg1{
+
+	public static void main(String[] args) {
+		
+	
+		
+	}
+	
+	public static int nummersumma(int... siffror){
+		
+		return 0;
+		
+	}
+		
+	
+		
+}
